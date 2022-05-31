cb354232135424f981bf57120bbab3ea1b225d95
Unprincess17 <202030211121@whu.edu.cn>
2022-05-31 11:54:52 +0800

    新增了配置类。包含数据库的地址信息

---
+++ Schelendar/Models/config.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schelendar.Models
+{
+    public static class config
+    {
+        public static string UserDBFile { get; set; }
+        public static string CourseDBFile { get; set; }
+        public static string TaskDBFile { get; set; }
+
+        static config()
+        {
+            UserDBFile = "SchInfos.db";
+            CourseDBFile = "SchInfos.db";
+            TaskDBFile = "SchInfos.db";
+
+        }
+
+    }
+}
