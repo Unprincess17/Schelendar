2d6129bbc8c880b7f05a0e14bdb869df609dd965
Zhang Hao <zh975525512@163.com>
2022-06-24 23:19:49 +0800

    事件显示完成

--- Schelendar/CalendarForm.cs
+++ Schelendar/CalendarForm.cs
@@ -1,13 +1,42 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 using Sunny.UI;
 
 namespace Schelendar
 {
     public partial class CalendarForm : UIForm
     {
+        class Task
+        {
+            public String Name { get; set; }
+            public DateTime Time { get; set; }
+            
+        }
+        
         public CalendarForm()
         {
             InitializeComponent();
+            // TODO: 添加事件的查询，返回列表类型？
+            List<Task> courseTemplates = null;
+            
+            foreach (var courseTemplate in courseTemplates)
+            {
+                UILabel uiLabel = new UILabel();
+                UIDatetimePicker datetimePicker = new UIDatetimePicker();
+                uiLabel.Text = courseTemplate.Name;
+                uiLabel.Dock = DockStyle.Fill;
+                uiLabel.TextAlign = ContentAlignment.MiddleRight;
+                datetimePicker.Value = courseTemplate.Time;
+                datetimePicker.Dock = DockStyle.Fill;
+                datetimePicker.ReadOnly = true;
+                templateTable.Controls.Add(uiLabel, 0, templateTable.RowCount-1);
+                templateTable.Controls.Add(datetimePicker, 1, templateTable.RowCount-1);
+                templateTable.RowStyles.Add(new RowStyle(SizeType.Absolute, 40));
+                templateTable.RowCount++;
+            }
+            
         }
     }
 }
